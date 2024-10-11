29dac2aea4ffc456377744704f1eff548d91e7f5
Pedro Lopez <pjlopezb@gmail.com>
2024-10-11 10:37:57 +0200

    Video 101

---
+++ foodies/components/Navbar/Navbar.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import Image from "next/image";
+import logo from "@/assets/logo.png";
+
+import Styles from "./Navbar.module.css";
+
+const Navbar = () => {
+	return (
+		<header className={Styles.header}>
+			<Link className={Styles.logo} href="/">
+				<Image src={logo} alt="Foodies logo" />
+				NextLevel food
+			</Link>
+			<nav className={Styles.nav}>
+				<ul>
+					<li>
+						<Link href="/meals">Browse Meals</Link>
+					</li>
+					<li>
+						<Link href="/community">Community</Link>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
+};
+
+export default Navbar;
