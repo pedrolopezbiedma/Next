992fe6d596da012e788c67a9978e12cdef9378ec
Pedro Lopez <pjlopezb@gmail.com>
2024-10-18 11:15:24 +0200

    Video 103

--- foodies/app/page.tsx
+++ foodies/app/page.tsx
@@ -1,9 +1,50 @@
+import Link from "next/link";
+
+import Styles from "./page.module.css";
+
 export default function Home() {
-  return (
-    <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
-        Time to get started!
-      </h1>
-    </main>
-  );
+	return (
+		<>
+			<header className={Styles.header}>
+				<div className={Styles.slideshow}></div>
+				<div>
+					<div className={Styles.hero}>
+						<h1>NextLevel foor for NextLevel foodies</h1>
+						<p>Taste and share food from all over the world</p>
+					</div>
+					<div className={Styles.cta}>
+						<Link href="/">Join the community</Link>
+						<Link href="/meals">Explore meals</Link>
+					</div>
+				</div>
+			</header>
+			<main>
+				<section className={Styles.section}>
+					<h2>How it works</h2>
+					<p>
+						NextLevel Food is a platform for foodies to share their favorite
+						recipes with the world. It&apos;s a place to discover new dishes,
+						and to connect with other food lovers.
+					</p>
+					<p>
+						NextLevel Food is a place to discover new dishes, and to connect
+						with other food lovers.
+					</p>
+				</section>
+
+				<section className={Styles.section}>
+					<h2>Why NextLevel Food?</h2>
+					<p>
+						NextLevel Food is a platform for foodies to share their favorite
+						recipes with the world. It&apos;s a place to discover new dishes,
+						and to connect with other food lovers.
+					</p>
+					<p>
+						NextLevel Food is a place to discover new dishes, and to connect
+						with other food lovers.
+					</p>
+				</section>
+			</main>
+		</>
+	);
 }
