bd71e6d70a82fb4f52d5312464c48d89c32b5962
Pedro Lopez <pjlopezb@gmail.com>
2024-10-19 17:48:05 +0200

    Video 114

--- foodies/app/meals/[mealId]/page.tsx
+++ foodies/app/meals/[mealId]/page.tsx
@@ -1,7 +1,34 @@
-const MealPage = () => {
+import Image from "next/image";
+import { getMealById } from "backend/meals";
+
+import Styles from "./page.module.css";
+
+const MealPage = ({ params }) => {
+	const { mealId } = params;
+	const meal = getMealById(mealId);
+	meal.instructions = meal.instructions.replace(/\n/g, "<br />");
 	return (
-		<main>
-			<h1>Meal Page</h1>
-		</main>
+		<>
+			<header className={Styles.header}>
+				<div className={Styles.image}>
+					<Image src={meal.image} alt="Meal image" fill />
+				</div>
+				<div className={Styles.headerText}>
+					<h1>{meal.title}</h1>
+					<p className={Styles.creator}>
+						<a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+					</p>
+					<p className={Styles.summary}>{meal.summary}</p>
+				</div>
+			</header>
+			<main>
+				<p
+					className={Styles.instructions}
+					dangerouslySetInnerHTML={{ __html: meal.instructions }}
+				/>
+			</main>
+		</>
 	);
 };
+
+export default MealPage;
--- foodies/backend/meals.ts
+++ foodies/backend/meals.ts
@@ -5,6 +5,9 @@ const db = sql("meals.db");
 
 export const getMeals = async (): Promise<Meal[]> => {
 	await new Promise((resolve) => setTimeout(resolve, 2000)); // Only added for demo purposes.
-	throw new Error();
-	// return db.prepare("SELECT * FROM meals").all();
+	return db.prepare("SELECT * FROM meals").all();
+};
+
+export const getMealById = (slug: string): Meal => {
+	return db.prepare("SELECT * from meals WHERE slug = ?").get(slug);
 };
