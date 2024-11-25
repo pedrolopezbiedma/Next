967055d18010e2d132f5484fe1121622db448d38
Pedro Lopez <pjlopezb@gmail.com>
2024-11-25 21:17:06 +0100

    Video 241

--- section-11/pages/clients/index.tsx
+++ section-11/pages/clients/index.tsx
@@ -1,9 +1,22 @@
+import Link from "next/link";
 import React from "react";
 
 const ClientsPage = () => {
+	const clients = [
+		{ id: "1", name: "Client One" },
+		{ id: "2", name: "Client Two" },
+		{ id: "3", name: "Client Three" },
+	];
 	return (
 		<div>
 			<h1>Clients Page</h1>
+			<ul>
+				{clients.map((client) => (
+					<li key={client.id}>
+						<Link href={`/clients/${client.id}`}>{client.name}</Link>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 };
