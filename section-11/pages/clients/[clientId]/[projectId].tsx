6c60c9bec645d3875b6b1f0b1fc2a9ad625fbc3c
Pedro Lopez <pjlopezb@gmail.com>
2024-11-25 21:09:35 +0100

    Video 237

---
+++ section-11/pages/clients/[clientId]/[projectId].tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ClientProjectDetails = () => {
+	return (
+		<div>
+			<h1>Client Project Details Page</h1>
+		</div>
+	);
+};
+
+export default ClientProjectDetails;
---
+++ section-11/pages/clients/[clientId]/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ClientDetailsPage = () => {
+	return (
+		<div>
+			<h1>Client Details Page</h1>
+		</div>
+	);
+};
+
+export default ClientDetailsPage;
