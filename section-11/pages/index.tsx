9b8f19b938f3deb314ec86a7dd0f426ed7591c25
Pedro Lopez <pjlopezb@gmail.com>
2024-11-25 21:14:47 +0100

    Video 239

--- section-11/pages/index.tsx
+++ section-11/pages/index.tsx
@@ -1,9 +1,18 @@
+import Link from "next/link";
 import React from "react";
 
 const HomePage = () => {
 	return (
 		<div>
 			<h1>Home Page</h1>
+			<ul>
+				<li>
+					<Link href={"/portfolio"}>My Portfolio</Link>
+				</li>
+				<li>
+					<Link href={"/clients"}>My Clients</Link>
+				</li>
+			</ul>
 		</div>
 	);
 };
