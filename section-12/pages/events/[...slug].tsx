7ed6b7e7c76f47aadd61cbbb6acc255b97d27993
Pedro Lopez <pjlopezb@gmail.com>
2024-11-26 21:46:22 +0100

    Last videos from section

--- section-12/pages/events/[...slug].tsx
+++ section-12/pages/events/[...slug].tsx
@@ -1,9 +1,24 @@
+import { useRouter } from "next/router";
+import { getFilteredEvents } from "../../dummy-data";
+import EventList from "../../components/EventList/EventList";
+
 const FilteredEventsPage = () => {
-	return (
-		<div>
-			<h1>Filtered Events Page</h1>
-		</div>
-	);
+	const router = useRouter();
+	const slug = router.query.slug;
+	const year = slug ? slug[0] : null;
+	const month = slug ? slug[1] : null;
+
+	if (!year && !month) {
+		return <p>Invalid filter, please refine your search.</p>;
+	}
+
+	const filteredEvents = getFilteredEvents({
+		year: +year,
+		month: +month,
+	});
+	console.log("🚀 ~ FilteredEventsPage ~ filteredEvents:", filteredEvents);
+
+	return <div>{filteredEvents && <EventList events={filteredEvents} />}</div>;
 };
 
 export default FilteredEventsPage;
